68e5b3ec25634243f143b92ce04625c1014bdad7
Alice Fung <alice.o.fung@gmail.com>
2015-07-03 13:06:29 -0400

    warmup_spec double_size method tests

--- spec/warmup_spec.rb
+++ spec/warmup_spec.rb
@@ -1,17 +1,41 @@
 require 'warmup'
+#require 'pry'
 
 describe Warmup do
 	let(:a){Warmup.new}
 	describe '#gets_shout' do
 
 		it 'accepts an input through gets' do
-			expect(a).to receive(:gets).and_return("HELLO")
-			a.gets_shout
+
+			#stub gets with lowercase
+			#expect an upcase return
+			allow(a).to receive(:gets).and_return("hello")
+			expect(a.gets_shout).to eq("HELLO")
+
 		end
 
 	end
 
 	describe '#double_size' do
+
+		it 'calls size method on argument' do
+			array2 = double("d", :size => 2)
+			expect(array2).to receive(:size).and_return(2)
+#			binding.pry
+			a.double_size(array2)
+		end
+
+		it 'returns double the size of the array given' do
+			array2 = double("d", :size => 2)
+			expect(a.double_size(array2)).to eq(array2.size*2)
+
+		end
+
+		it"random" do
+			expect{a.double_size}.to raise_error
+			puts " "
+		end
+
 
 	end
 
