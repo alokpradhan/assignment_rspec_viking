7f5ba3513ce18ea68fb66d4239e8e01b0a7f9354
alokp <alokpra@gmail.com>
2015-07-03 13:59:14 -0700

    pick up and drop weapon tests complete

--- spec/bow_spec.rb
+++ spec/bow_spec.rb
@@ -5,7 +5,6 @@ subclasses of weapon: bow, axe
 viking
 =end
 
-
 require_relative '../lib/weapons/bow.rb'
 
 describe Bow do
@@ -13,43 +12,30 @@ describe Bow do
   let(:bow){Bow.new}
 
   describe "#initialize" do
-
     specify "arrow count default as 10" do
-
       expect(bow.arrows).to eq(10)
-
     end
 
     specify "arrows as readable" do
-
       expect(bow.arrows).to be_a(Integer)
-
     end
 
     it "starts with the number of arrows as specified" do
-
       expect(Bow.new(5).arrows).to eq(5)
-
     end
-
-
   end
 
   describe "#use" do
-
     it 'reduces arrow count by 1' do
       # expectations = bow.arrows
       # result = bow.arrows - 1
       bow.use
       expect(bow.arrows).to eq(9)
-
     end
 
     it 'raises error when out_of_arrows' do
       bow2 = Bow.new(0)
       expect{bow2.use}.to raise_error("Out of arrows")
     end
-
   end
-
 end
--- spec/viking_spec.rb
+++ spec/viking_spec.rb
@@ -5,6 +5,8 @@ require_relative '../lib/weapons/weapon.rb'
 describe Viking do
 
 	let(:viking){Viking.new("Random", 75, 20)}
+	let(:axe){Axe.new}
+	let(:bow){Bow.new}
 
 	describe '#initialize' do
 
@@ -22,26 +24,50 @@ describe Viking do
 
 		it 'health cannot be overwritten after it has been set' do
 			#viking.instance_variable_set(:@health, 100)
-
 			expect{viking.health=100}.to raise_error
 		end
 
 	end
 
 	describe '#pick_up_weapon' do
-
 		it "should set weapon for viking" do
-			knife = Axe.new
 			#knife = double("Axe", :superclass => Weapon)
-			viking.pick_up_weapon(knife)
-			expect(viking.weapon).to eq(knife)
-
+			viking.pick_up_weapon(axe)
+			expect(viking.weapon).to eq(axe)
 		end
 
-		it "should raise exception when picking up non-weapon"
+		it "should raise exception when picking up non-weapon" do
+			knife = instance_double("Axe", :=== => Weapon)   #double doesn't mimic the actual class
+			expect{viking.pick_up_weapon(knife)}.to raise_error("Can't pick up that thing")
+		end
 
-		it "should replace existing weapon with new weapon"
+		it "should replace existing weapon with new weapon" do
+			viking.pick_up_weapon(axe)
+			expect(viking.weapon.name).to eq("Axe")
+			viking.pick_up_weapon(bow)
+			expect(viking.weapon).to be_a(Bow)
+		end
 
+		describe '#drop_weapon' do
+			specify 'weapon should be nil' do
+				viking.pick_up_weapon(axe)
+				viking.drop_weapon
+				expect(viking.weapon).to be_nil
+			end
+		end
+
+		describe '#receive_attack' do
+			it 'reduces vikings health by specified amount' do
+			end
+
+			it "causes health of attacked viking to drop"
+			it "causes attacked viking to call #take_damage"
+			specify "attacking with no weapon to run #damage_with_fists"
+			specify "attacking with weapon to run #damage_with_weapon"
+			specify "attacking with weapon deals damage equal to Viking's strength * Weapon's multiplier"
+			specify "attacking with Bow without arrows uses Fists instead"
+
+		end
 	end
 
 end
