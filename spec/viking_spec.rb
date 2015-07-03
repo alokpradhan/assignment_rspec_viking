0da1056bc9826041f4e33e9ad0ec39d4c7647254
Alice Fung <alice.o.fung@gmail.com>
2015-07-03 15:06:17 -0400

    start viking spec tests

--- spec/viking_spec.rb
+++ spec/viking_spec.rb
@@ -1,10 +1,12 @@
 require 'viking'
+require_relative '../lib/weapons/axe.rb'
+require_relative '../lib/weapons/weapon.rb'
 
 describe Viking do
 
-	describe '#initialize' do
+	let(:viking){Viking.new("Random", 75, 20)}
 
-		let(:viking){Viking.new("Random", 75, 20)}
+	describe '#initialize' do
 
 		it 'should create viking with given name' do
 			expect(Viking.new("Olaf").name).to eq("Olaf")
@@ -19,9 +21,26 @@ describe Viking do
 		end
 
 		it 'health cannot be overwritten after it has been set' do
-			viking.instance_variable_set(:@health, 100)
-			expect(viking.health).to be <= 75
+			#viking.instance_variable_set(:@health, 100)
+
+			expect{viking.health=100}.to raise_error
 		end
+
+	end
+
+	describe '#pick_up_weapon' do
+
+		it "should set weapon for viking" do
+			knife = Axe.new
+			#knife = double("Axe", :superclass => Weapon)
+			viking.pick_up_weapon(knife)
+			expect(viking.weapon).to eq(knife)
+
+		end
+
+		it "should raise exception when picking up non-weapon"
+
+		it "should replace existing weapon with new weapon"
 
 	end
 
